b04861469801c0414da132df3c6e274571faf8d0
alkygerolymatou <alky.gerolymatou@gmail.com>
2026-01-21 23:08:45 +0100

    Session 3-4 hw

---
+++ session 3-4 exerise.py
@@ -0,0 +1,19 @@
+print("Hello, This is exerise for session 3,4")
+print("we can print a list of items: dog", "cat", "mouse", "elephant", "moose", sep="!!   ",
+      end="! just one exclamation for moose\n")
+print("**Important**")
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+print("Here is the list for the supermarket:")
+items = ["milk", "bread", "eggs", "yogurt", "apples"]
+for item in items:
+    print(item)
+print("Lets say a=12")
+a= 12
+print("How much is a+29?")
+print(a+29)
+b= 10
+print("Lets now say that there is also a b=10")
+print("How much is a*b?")
+print(a*b)
+
